dd3d1d9913aaa936ee88a84c3851016efee5fbe4
arthur851 <arthurms10326@gmail.com>
2026-06-26 11:49:45 -0300

    cadastro e alterar senha
    criação do front-end dessas páginas e atualizações no css

---
+++ front-end/alterar senha/script.js
@@ -0,0 +1,4 @@
+const tamanho = 10;
+const caracteres = Math.random().toString(36).substring(2, 2 + tamanho);
+
+console.log(caracteres);
---
+++ front-end/login/script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('form');
+const inpEmail = document.querySelector('#email');
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+});
