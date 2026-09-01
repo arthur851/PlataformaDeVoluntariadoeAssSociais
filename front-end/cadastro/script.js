0564f62e089218560c641aa9aff91fad17cf03b0
Lety-s <letycia.silva@escolar.ifrn.edu.br>
2026-09-01 20:46:42 -0300

    DOM Cadastro

---
+++ front-end/cadastro/script.js
@@ -0,0 +1,19 @@
+const BASE_URL = "http://localhost:3000"
+
+const cadastroForm = document.querySelector(".form-box");
+
+const inputNome = document.querySelector("#nome")
+const inputCPF = document.querySelector("#cpf")
+const inputTelefone= document.querySelector("#telefone")
+const inputEmail= document.querySelector("#email")
+const inputSenha= document.querySelector("#senha")
+const inputConfirmarSenha= document.querySelector("#confirmar-senha")
+
+cadastroForm.addEventListener("submit", validarCadastro);
+
+async function validarCadastro() {
+    
+    
+}
+
+
