d2ebbbba23bb4c91b5bce5c1b18fb6d0d85ef14a
Lety-s <letycia.silva@escolar.ifrn.edu.br>
2026-09-02 13:34:26 -0300

    DOM paginas de formulário

---
+++ front-end/alterar senha/script.js
@@ -0,0 +1,11 @@
+const BASE_URL = "http://localhost:3000"
+
+const AlterarForm = document.querySelector(".form-box");
+
+const inputEmail = document.querySelector("#email")
+const inputCodigoV = document.querySelector("#Vcode")
+
+
+
+
+
